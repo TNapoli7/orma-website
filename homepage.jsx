f1f6d0d6abbc4027200499b1061af411c44de95b
Tomas Napoles <napoles.tomas@gmail.com>
2026-05-15 02:40:39 +0100

    fix: add screen blend mode to eliminate black JPEG background

--- homepage.jsx
+++ homepage.jsx
@@ -1111,6 +1111,7 @@ function Pillars() {
             backgroundPosition: 'center',
             opacity: 0.82,
             filter: 'sepia(1) saturate(0.15) hue-rotate(60deg) brightness(0.33)',
+            mixBlendMode: 'screen',
           }} aria-hidden="true" />
         </div>
       </div>
